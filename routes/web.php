d6756cc50e21aa77c807a0ea7c2b662fa7730711
Jerick Dela Rosa <delarosajerick26@gmail.com>
2022-06-25 00:45:21 +0800

    LARAVEL RESET
    DO NOT MERGE ANY BRANCH HERE @MAIN

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('sodas/index');
+    return view('welcome');
 });
-
-
-Route::get('/', [SodasController::class, 'get']);
